6ef0d394f50d549f27f527e72beaa006d9bab4ea
Diego Dominguez <diegodominguezf40@gmail.com>
2023-12-26 17:00:55 -0700

    in the works

---
+++ server/controllers/FilterController.js
@@ -0,0 +1,104 @@
+import BaseController from "../utils/BaseController";
+import { logger } from "../utils/Logger";
+
+export class FilterController extends BaseController{
+    constructor(){
+        super('api/filter')
+        this.router
+        // .get('/accounts', this.filterAccounts)
+        // .get('/category', this.filterCategories)
+        // .get('/departments', this.filterDepartments)
+        // .get('/labels', this.filterLabels)
+        .get('/label-defects', this.filterLabelDefects)
+        // .get('/materials', this.filterMaterials)
+        // .get('/orders', this.filterOrders)
+        // .get('/archived-orders', this.filterArchivedOrders)
+        // .get('sub-category', this.filterSubCategories)
+    }
+
+
+    // async filterAccounts(req, res, next){
+    //     try {
+            
+    //     } catch (error) {
+    //         logger.error(error)
+    //     }
+    // }
+
+
+    // async filterCategories(req, res, next){
+    //     try {
+    //         res.send({value: "THIS IS WORKING"})
+    //     } catch (error) {
+    //         logger.error(error)
+    //     }
+    // }
+
+
+    // async filterDepartments(req, res, next){
+    //     try {
+            
+    //     } catch (error) {
+    //         logger.error(error)
+    //     }
+    // }
+
+
+    // async filterLabels(req, res, next){
+    //     try {
+    //         const dataType = req.params.dataType
+    //         logger.log(dataType)
+    //         res.send({value: "THIS IS WORKING"})
+    //     } catch (error) {
+    //         logger.error(error)
+    //     }
+    // }
+
+
+    async filterLabelDefects(req, res, next){
+        try {
+
+            res.send({value: "THIS IS WORKING"})
+        } catch (error) {
+            logger.error(error)
+            next(error)
+        }
+    }
+
+
+    // async filterMaterials(req, res, next){
+    //     try {
+            
+    //     } catch (error) {
+    //         logger.error(error)
+    //     }
+    // }
+
+
+    // async filterOrders(req, res, next){
+    //     try {
+            
+    //     } catch (error) {
+    //         logger.error(error)
+    //     }
+    // }
+
+
+    // async filterArchivedOrders(req, res, next){
+    //     try {
+            
+    //     } catch (error) {
+    //         logger.error(error)
+    //     }
+    // }
+
+
+    // async filterSubCategories(req, res, next){
+    //     try {
+            
+    //     } catch (error) {
+    //         logger.error(error)
+    //     }
+    // }
+
+}
